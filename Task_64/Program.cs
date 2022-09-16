aed5eeb610158a6de771e54047ada8006578a1b5
Андрей <pjanov84@mail.ru>
2022-09-16 21:00:28 +0300

    Added Task_64

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -1,10 +1,21 @@
 ﻿//                                 Задача 64: 
 // Задайте значения M и N. Напишите программу, которая выведет 
 // все натуральные числа в промежутке от M до N.
-
 //  M = 1; N = 5. -> ""1, 2, 3, 4, 5""
-//  M = 4; N = 8. -> ""4, 6, 7, 8""
+//  M = 4; N = 8. -> ""4, 6, 7, 8"" // В примере пропущена пятерка )))
 
+Console.Write("Введи M: ");
+int M = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
 
+string NumbersNatural(int M, int N) // натуральные числа в промежутке от M до N:
+{
+    if (M <= N)
+    return $"{M}{(M != N ? "," : "")} " + NumbersNatural(M + 1, N);
+    else return String.Empty;
+}
 
+Console.WriteLine($"M = {M}; N = {N}. -> {(NumbersNatural(M, N))}");
 
+// А кавычки мне так и не удалось реализовать!)))
